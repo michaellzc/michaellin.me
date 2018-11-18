4d35d9883b629889642f08cda0b3fac84c9caade
Michael Lin <michael.lin1@ibm.com>
2018-11-17 19:01:03 -0700

    feat: add menu to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import Link from './link'
 
 const Footer = () => (
   <footer className="footer has-background-light">
-    <div className="content has-text-centered">
-      <p>Copyrights © 2018 Michael Lin</p>
-    </div>
+    <nav className="container level">
+      <div className="level-left">
+        <Link to="/" className="button is-text level-item">
+          Home
+        </Link>
+        <Link to="/wip" className="button is-text level-item">
+          Blog
+        </Link>
+        <Link to="/wip" className="button is-text level-item">
+          About
+        </Link>
+      </div>
+
+      <div className="level-right">
+        <div className="has-text-centered">
+          <p>Copyrights © 2018 Michael Lin</p>
+        </div>
+      </div>
+    </nav>
   </footer>
 )
 
