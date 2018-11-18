b2166567fa234475bf001509fcd44fd60dfcec89
Michael Lin <michael.lin1@ibm.com>
2018-11-18 00:16:26 -0700

    style: clean up unused variable

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,8 @@
 import React from 'react'
-import { css } from 'emotion'
 import Layout from '../components/hero-layout'
-import Footer from '../components/footer'
 import Intro from '../components/intro'
 import Contact from '../components/contact'
 import Menu from '../components/menu'
-
-const fixFullHeight = css`
-  .hero.is-fullheight.has-fixed-navbar {
-    min-height: calc(100vh - #{$navbar-height});
-  }
-`
 
 const IndexPage = () => (
   <Layout>
